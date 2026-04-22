62b9c1bc6ba52d6198c3c17858caed6db559d03c
Yanick Schraner <yanick.schraner@bs.ch>
2026-04-22 14:41:10 +0200

    fix entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 #!/bin/sh
 set -e
 
-FORCE_COLOR=1
-varlock run -- fastapi run /app/src/transcribo_backend/app.py --host 0.0.0.0 --port "${PORT:-8090}"
+FORCE_COLOR=1 varlock run -- fastapi run /app/src/transcribo_backend/app.py --host 0.0.0.0 --port "${PORT:-8090}"
